11b75943bf883f077db87058e2a3f3d7b0d26ae5
scottsimply <scottistriggered@gmail.com>
2023-08-24 12:41:07 -0500

    Start of new weapon
    Kinda adding the EPC.

---
+++ Content/Items/Weapons/PlasmaPistol.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace deeprockitems.Content.Items.Weapons
+{
+    public class PlasmaPistol : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 15;
+            Item.DamageType = DamageClass.Magic;
+            Item.mana = 8;
+            Item.knockBack = 4;
+            Item.crit = 4;
+            Item.useTime = 5;
+            Item.useAnimation = 5;
+        }
+    }
+}
