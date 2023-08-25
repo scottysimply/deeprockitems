0905f856aca43bcbb3eba47ed382ba1104430b4b
scottsimply <scottistriggered@gmail.com>
2023-08-25 07:28:56 -0500

    Merging progress
    Doing so that I can work on this on my desktop.

--- Content/Items/Weapons/PlasmaPistol.cs
+++ Content/Items/Weapons/PlasmaPistol.cs
@@ -1,19 +1,31 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace deeprockitems.Content.Items.Weapons
 {
-    public class PlasmaPistol : ModItem
+    public class PlasmaPistol : UpgradeableItemTemplate
     {
-        public override void SetDefaults()
+        public override void SafeDefaults()
         {
             Item.damage = 15;
+            Item.rare = ItemRarityID.Green;
             Item.DamageType = DamageClass.Magic;
             Item.mana = 8;
             Item.knockBack = 4;
             Item.crit = 4;
-            Item.useTime = 5;
-            Item.useAnimation = 5;
+            Item.useTime = 13;
+            Item.useAnimation = 13;
+            Item.shoot = ProjectileID.PurificationPowder;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.shootSpeed = 10f;
+            Item.UseSound = SoundID.Item8;
+        }
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            type = ModContent.ProjectileType<Content.Projectiles.PlasmaProjectiles.PlasmaBullet>();
         }
     }
 }
---
+++ Content/Projectiles/PlasmaProjectiles/PlasmaBullet.cs
@@ -0,0 +1,16 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace deeprockitems.Content.Projectiles.PlasmaProjectiles
+{
+    public class PlasmaBullet : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 16;
+            Projectile.height = 16;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+        }
+    }
+}
