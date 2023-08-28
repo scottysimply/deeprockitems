cbfda09aeed87b39a5519e5d729b76dc6c45ec3b
scottsimply <scottistriggered@gmail.com>
2023-08-28 07:28:39 -0500

    Sound IDs are now a modsystem

--- Assets/Sounds/DRGSoundIDs.cs
+++ Assets/Sounds/DRGSoundIDs.cs
@@ -1,8 +1,9 @@
 ﻿using Terraria.Audio;
+using Terraria.ModLoader;
 
 namespace deeprockitems.Assets.Sounds
 {
-    public static class DRGSoundIDs
+    public class DRGSoundIDs : ModSystem
     {
         public static SoundStyle M1000Focus { get; private set; }
         public static SoundStyle M1000Fire { get; private set; }
@@ -10,8 +11,7 @@ namespace deeprockitems.Assets.Sounds
         public static SoundStyle SludgePumpFire { get; private set; }
         public static SoundStyle SludgeBallHit { get; private set; }
         
-
-        public static void InitializeSounds()
+        public override void OnModLoad()
         {
             string dir = "deeprockitems/Assets/Sounds/";
             M1000Focus = new SoundStyle(dir + "Items/M1000Focus");
@@ -19,7 +19,6 @@ namespace deeprockitems.Assets.Sounds
             SludgePumpFocus = new SoundStyle(dir + "Items/SludgePumpFocus");
             SludgePumpFire = new SoundStyle(dir + "Items/SludgePumpFire");
             SludgeBallHit = new SoundStyle(dir + "Projectiles/SludgeBallHit");
-
         }
     }
 
