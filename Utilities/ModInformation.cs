06316ef5d8c8fab136bf5227255c011ea03e9698
scottsimply <scottistriggered@gmail.com>
2023-08-28 07:34:36 -0500

    New utility file

---
+++ Utilities/ModInformation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace deeprockitems.Utilities
+{
+    public class ModInformation
+    {
+        /// <summary>
+        /// Gets whether the given projectile type is from vanilla or not.
+        /// </summary>
+        /// <param name="type">The projectile ID.</param>
+        /// <returns></returns>
+        public static bool IsProjectileVanilla(int type)
+        {
+            return type <= ProjectileID.Count;
+        }
+        public static bool IsProjectileMyMod(int type)
+        {
+            if (ProjectileLoader.GetProjectile(type) is null)
+            {
+                return false;
+            }
+            return ProjectileLoader.GetProjectile(type).Mod == ModContent.GetInstance<deeprockitems>();
+        }
+    }
+}
