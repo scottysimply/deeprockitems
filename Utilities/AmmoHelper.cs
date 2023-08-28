a79a939da18c99c830097e4c646eab8bfc60f444
scottsimply <scottistriggered@gmail.com>
2023-08-28 07:30:53 -0500

    Ammo ID helper

---
+++ Utilities/AmmoHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+
+namespace deeprockitems.Utilities
+{
+    public static class AmmoHelper
+    {
+        public static int AmmoID_GetProjectileID(int itemID)
+        {
+            Dictionary<int, Item> Items = ContentSamples.ItemsByType;
+            Item AmmoUsed = ContentSamples.ItemsByType[itemID];
+
+            return AmmoUsed.shoot;
+        }
+    }
+}
