b05315b3020684367171777533814c1ce1cab6ba
scottsimply <scottistriggered@gmail.com>
2023-08-28 20:38:27 -0500

    New util
    Used to find if two projectiles are colliding

---
+++ Utilities/ModProjectileExtension.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace deeprockitems.Utilities
+{
+    public static class ProjectileExtension
+    {
+        /// <summary>
+        /// Determines if the given projectile is colliding with a desired projectile type.
+        /// </summary>
+        /// <param name="projectile"></param>
+        /// <param name="target_ID"></param>
+        /// <returns>Returns the instance of the colliding projectile. Returns null if no collision occured.</returns>
+        public static Projectile? IsCollidingWithProjectile(this Projectile projectile, int target_ID)
+        {
+            foreach (Projectile proj in Main.projectile)
+            {
+                if (proj.type == target_ID)
+                {
+                    continue;
+                }
+                if (projectile.Hitbox.Intersects(proj.Hitbox))
+                {
+                    return proj;
+                }
+            }
+            return null;
+        }
+    }
+}
