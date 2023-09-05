9a5a5b3d3b1bad7819a9a2a9bae9ca112284f887
scottsimply <scottistriggered@gmail.com>
2023-09-04 21:36:15 -0500

    EPC overclock changes

--- Content/Projectiles/PlasmaProjectiles/PlasmaExplosion.cs
+++ Content/Projectiles/PlasmaProjectiles/PlasmaExplosion.cs
@@ -1,10 +1,16 @@
 ﻿using Terraria.ModLoader;
 using Terraria;
+using Terraria.DataStructures;
+using deeprockitems.Content.Items.Upgrades.PlasmaPistol;
+using deeprockitems.Content.Items.Weapons;
+using deeprockitems.Utilities;
 
 namespace deeprockitems.Content.Projectiles.PlasmaProjectiles
 {
     public class PlasmaExplosion : ModProjectile
     {
+        private int[] upgrades = new int[4];
+        private bool? canDamage = null;
         public override void SetStaticDefaults()
         {
             Main.projFrames[Projectile.type] = 3;
@@ -30,6 +36,37 @@ namespace deeprockitems.Content.Projectiles.PlasmaProjectiles
             {
                 Projectile.Kill();
             }
+        }
+        public override void OnSpawn(IEntitySource source)
+        {
+            if (source is EntitySource_ItemUse { Item.ModItem: UpgradeableItemTemplate parentItem })
+            {
+                upgrades = parentItem.Upgrades;
+            }
+            if (upgrades.Contains(ModContent.ItemType<MountainMoverOC>()))
+            {
+                canDamage = false;
+                if (Projectile.owner == Main.myPlayer)
+                {
+
+                    int explosionRadius = (int)(Projectile.height / 32f);
+                    int minTileX = (int)(Projectile.Center.X / 16f - explosionRadius);
+                    int maxTileX = (int)(Projectile.Center.X / 16f + explosionRadius);
+                    int minTileY = (int)(Projectile.Center.Y / 16f - explosionRadius);
+                    int maxTileY = (int)(Projectile.Center.Y / 16f + explosionRadius);
+
+                    // Ensure that all tile coordinates are within the world bounds
+                    Utils.ClampWithinWorld(ref minTileX, ref minTileY, ref maxTileX, ref maxTileY);
+
+                    // These 2 methods handle actually mining the tiles and walls while honoring tile explosion conditions
+                    bool explodeWalls = Projectile.ShouldWallExplode(Projectile.Center, explosionRadius, minTileX, maxTileX, minTileY, maxTileY);
+                    Projectile.ExplodeTiles(Projectile.Center, explosionRadius, minTileX, maxTileX, minTileY, maxTileY, explodeWalls);
+                }
+            }
+        }
+        public override bool? CanDamage()
+        {
+            return canDamage;
         }
     }
 }
