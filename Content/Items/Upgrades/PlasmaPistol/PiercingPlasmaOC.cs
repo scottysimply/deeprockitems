922c85701073812472e52d5ae5cdb8d144cb29dd
scottsimply <scottistriggered@gmail.com>
2023-09-04 22:41:30 -0500

    fixed rarity

--- Content/Items/Upgrades/PlasmaPistol/PiercingPlasmaOC.cs
+++ Content/Items/Upgrades/PlasmaPistol/PiercingPlasmaOC.cs
@@ -6,13 +6,11 @@ namespace deeprockitems.Content.Items.Upgrades.PlasmaPistol
 {
     public class PiercingPlasmaOC : UpgradeTemplate
     {
-        public override string ItemName { get => "Digger Rounds"; set => base.ItemName = value; }
-        public override string ItemTooltip { get => "Focus shots fire through tiles"; set => base.ItemTooltip = value; }
         public override bool IsOverclock { get => true; set => base.IsOverclock = value; }
         public override void SetDefaults()
         {
             base.SetDefaults();
-            Item.rare = ItemRarityID.Lime;
+            Item.rare = ItemRarityID.Yellow;
         }
     }
 }
