e680fb5198622a8bbfc7209665c083cdebce8d37
scottsimply <scottistriggered@gmail.com>
2023-09-20 20:54:39 -0500

    Indev: MULE pet
    Will act like chester, but will follow walls like the spider egg pet

---
+++ Content/Pets/MollyPetProjectile.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace deeprockitems.Content.Pets
+{
+    public class MollyPetProjectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = 
+        }
+        public override void SetDefaults()
+        {
+            Projectile.CloneDefaults(ProjectileID.BabySpider);
+            Projectile.width = 
+        }
+    }
+}
