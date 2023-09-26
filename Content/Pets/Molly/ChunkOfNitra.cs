9fc62560b058dbf0fca625acfb26245f2a1c5cf5
scottsimply <scottistriggered@gmail.com>
2023-09-26 08:44:56 -0500

    fixed item

--- Content/Pets/Molly/ChunkOfNitra.cs
+++ Content/Pets/Molly/ChunkOfNitra.cs
@@ -7,15 +7,14 @@ namespace deeprockitems.Content.Pets.Molly
 {
     public class ChunkOfNitra : ModItem
     {
-        // Names and descriptions of all ExamplePetX classes are defined using .hjson files in the Localization folder
         public override void SetDefaults()
         {
-            Item.CloneDefaults(ItemID.SpiderEgg); // Copy the Defaults of the Zephyr Fish Item.
+            Item.CloneDefaults(ItemID.SpiderEgg); // Essentially a copy of the spider pet.
             Item.width = 24;
             Item.height = 24;
 
-            Item.shoot = ModContent.ProjectileType<MollyPetProjectile>(); // "Shoot" your pet projectile.
-            Item.buffType = ModContent.BuffType<MollyPetBuff>(); // Apply buff upon usage of the Item.
+            Item.shoot = ModContent.ProjectileType<MollyPetProjectile>(); // Spawn molly
+            Item.buffType = ModContent.BuffType<MollyPetBuff>(); // Give pet buff
         }
 
         public override void UseStyle(Player player, Rectangle heldItemFrame)
