00d800046b3fb7350653fce27f922d1e849b262e
scottsimply <scottistriggered@gmail.com>
2023-09-26 06:33:34 -0500

    new extension, unused

--- Utilities/ArrayExtensions.cs
+++ Utilities/ArrayExtensions.cs
@@ -21,5 +21,46 @@ namespace deeprockitems.Utilities
             }
             return -1;
         }
+        public static T[] AddAndReplace<T>(this T[] array, T item, T replace = default)
+        {
+            if (!array.Contains(replace))
+            {
+                throw new System.ArgumentOutOfRangeException(nameof(array), $"{nameof(array)} did not contain {nameof(item)}.");
+            }
+            else
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i].Equals(replace))
+                    {
+                        array[i] = item;
+                        return array;
+                    }
+                }
+            }
+            return array;
+        }
+        public static T[] PopAndShuffle<T>(this T[] array, T item)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Equals(default(T)))
+                {
+                    array[i] = item;
+                    break;
+                }
+            }
+            int index = array.FindFirstIndexOf(default(T));
+            if (index == -1) return array;
+            
+            for (int i = index; i < array.Length - 1; i++)
+            {
+                if (array[i].Equals(default(T)))
+                {
+                    (array[i], array[i + 1]) = (array[i + 1], array[i]);
+                }
+            }
+            return array;
+        }
     }
 }
