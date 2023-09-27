8b1e2ba5433419dd18782b163111dccfad7aa842
scottsimply <scottistriggered@gmail.com>
2023-09-26 20:43:35 -0500

    Recipegroup added

--- Content/Recipes.cs
+++ Content/Recipes.cs
@@ -11,10 +11,12 @@ namespace deeprockitems
         {
             RecipeGroup AnyGold = new(() => $"{Language.GetTextValue("LegacyMisc.37")} {Lang.GetItemNameValue(ItemID.GoldBar)}", ItemID.GoldBar, ItemID.PlatinumBar);
             RecipeGroup.RegisterGroup(nameof(ItemID.GoldBar), AnyGold);
-            RecipeGroup AnyEvilBar = new(() => $"{Language.GetTextValue("LegacyMisc.37")} {Lang.GetItemNameValue(ItemID.DemoniteBar)} {Lang.GetItemNameValue(ItemID.Bar)}", ItemID.DemoniteBar, ItemID.CrimtaneBar);
+            RecipeGroup AnyEvilBar = new(() => $"{Language.GetTextValue("LegacyMisc.37")} {Lang.GetItemNameValue(ItemID.DemoniteBar)}", ItemID.DemoniteBar, ItemID.CrimtaneBar);
             RecipeGroup.RegisterGroup(nameof(ItemID.DemoniteBar), AnyEvilBar);
             RecipeGroup AnyPowder = new(() => $"{Language.GetTextValue("LegacyMisc.37")} {Lang.GetItemNameValue(ItemID.VilePowder)}", ItemID.VilePowder, ItemID.ViciousPowder);
             RecipeGroup.RegisterGroup(nameof(ItemID.VilePowder), AnyPowder);
+            RecipeGroup AnyTier1Ore = new(() => $"{Language.GetTextValue("LegacyMisc")} {Lang.GetItemNameValue(ItemID.CobaltBar)}", ItemID.CobaltBar, ItemID.PalladiumBar);
+            RecipeGroup.RegisterGroup(nameof(ItemID.CobaltBar), AnyTier1Ore);
         }
     }
 }
