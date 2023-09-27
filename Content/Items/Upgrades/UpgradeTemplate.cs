5f465add9ca2389161ee00c582bcca840e0271ba
scottsimply <scottistriggered@gmail.com>
2023-09-27 18:33:57 -0500

    Fixed tooltip

--- Content/Items/Upgrades/UpgradeTemplate.cs
+++ Content/Items/Upgrades/UpgradeTemplate.cs
@@ -7,18 +7,17 @@ using deeprockitems.Content.Items.Weapons;
 using Terraria.UI;
 using System.Linq;
 using System;
+using Microsoft.Xna.Framework;
 
 namespace deeprockitems.Content.Items.Upgrades
 {
     public abstract class UpgradeTemplate : ModItem
     {
-        public virtual string ItemName { get; set; }
-        public virtual string ItemTooltip { get; set; }
         public virtual bool IsOverclock { get; set; }
         /// <summary>
         ///  This is used for displaying what weapons the upgrade can be used on. I elected to use this syntax since it's slightly cleaner lol
         /// </summary>
-        public virtual List<int> ValidWeapons { get; set; }
+        public virtual List<int> ValidWeapons { get; set; } = new List<int>() { 0 };
         public override void SetDefaults()
         {
             Item.rare = ItemRarityID.Pink;
@@ -30,24 +29,28 @@ namespace deeprockitems.Content.Items.Upgrades
         }
         public override void ModifyTooltips(List<TooltipLine> tooltips)
         {
+            Color c = new(74, 177, 211);
             if (ItemSlot.ShiftInUse && ValidWeapons.Count > 0)
             {
-                string upgrades_text = Language.GetTextValue("Mods.deeprockitems.Misc.ValidUpgradesText");
+                string upgrades_text = Language.GetTextValue("Mods.deeprockitems.Misc.ValidUpgradesText", DisplayName);
                 foreach (int type in ValidWeapons)
                 {
-                    upgrades_text += $" {Lang.GetItemName(type)},";
+                    upgrades_text += " " + Lang.GetItemName(type) +",";
 
 
                 }
                 upgrades_text = upgrades_text.TrimEnd(',');
-                TooltipLine line = new(Mod, "Upgrades", upgrades_text);
+                TooltipLine line = new(Mod, "Upgrades", upgrades_text)
+                {
+                    OverrideColor = c,
+                };
                 tooltips.Add(line);
             }
             else if (ValidWeapons.Count > 0)
             {
-                TooltipLine line = new(Mod, "ShiftToView", Language.GetTextValue("Mods.deeprockitems.Misc.WeaponsShiftTooltip", Main.cTorch))
+                TooltipLine line = new(Mod, "ShiftToView", Language.GetTextValue("Mods.deeprockitems.Misc.WeaponShiftTooltip"))
                 {
-                    OverrideColor = new(74, 177, 211),
+                    OverrideColor = c,
                 };
                 tooltips.Add(line);
             }
