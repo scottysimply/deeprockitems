3b496789fdc077c9e72d480e1c3d2ac4064ab44c
scottsimply <scottistriggered@gmail.com>
2023-09-27 19:46:59 -0500

    Mining quests delete the item. sorry! couldn't think up a better way

--- Common/Quests/QuestsTileTracker.cs
+++ Common/Quests/QuestsTileTracker.cs
@@ -21,33 +21,22 @@ namespace deeprockitems.Common.Quests
             {
                 // Fixing all gem migraines in this section of code
                 int subID = FixGems(i, j, modPlayer.CurrentQuestInformation[1]);
+                // Essentially, test for gems
                 if (subID > -1 && Main.tile[i, j].TileType == TileID.ExposedGems && Main.tile[i, j].TileFrameX > 16 * subID && Main.tile[i, j].TileFrameX < 16 * (subID + 1) && !effectOnly && !fail)
                 {
                     modPlayer.CurrentQuestInformation[3]--;
                     QuestsBase.DecrementProgress(modPlayer);
+                    noItem = true;
                     return;
                 }
                 else if (modPlayer.CurrentQuestInformation[1] == type && !effectOnly && !fail) // Well. It wasn't the gem, or at least, not the correct one. Continue as normal
                 {
                     modPlayer.CurrentQuestInformation[3]--;
                     QuestsBase.DecrementProgress(modPlayer);
+                    noItem = true;
                     return;
                 }
-                
-
             }
-            /*// Trying anything I can to make sure this player is the one destroying the tile.
-            if (modPlayer.Player.IsInTileInteractionRange(i, j, Terraria.DataStructures.TileReachCheckSettings.Pylons))
-            {
-                // Check if quest is mining, and pertains to this tile
-                if (modPlayer.CurrentQuestInformation[0] == 1 && type2 == modPlayer.CurrentQuestInformation[1] && !effectOnly && !fail)
-                {
-                    modPlayer.CurrentQuestInformation[3]--;
-                    QuestsBase.DecrementProgress(modPlayer);
-                }
-            }*/
-
-
         }
         private static int FixGems(int i, int j, int questType)
         {
@@ -64,27 +53,4 @@ namespace deeprockitems.Common.Quests
             };
         }
     }
-   /* public class MiningDetour : ModSystem
-    {
-        public override void Load()
-        {
-            Terraria.Achievements
-        }
-        private void On_Main_DrawInterface_36_Cursor(On_Main.orig_DrawInterface_36_Cursor orig)
-        {
-            if (Main.cursorOverride != 30)
-            {
-                orig(); // Call original method
-            }
-            else
-            {
-                Main.spriteBatch.End();
-                Texture2D cursor = ModContent.Request<Texture2D>("deeprockitems/UI/UpgradeCursor").Value;
-
-                Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.SamplerStateForCursor, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, Main.UIScaleMatrix);
-                Main.spriteBatch.Draw(cursor, Main.MouseScreen, null, Color.White, 0, new Vector2(.1f) * cursor.Size(), Main.cursorScale * 1.1f, SpriteEffects.None, 0f);
-            }
-
-        }
-    }*/
 }
