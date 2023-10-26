3b29738eb19ccb761bcbd5f0e5b8c5aed8a4bdc3
scottsimply <scottistriggered@gmail.com>
2023-10-26 13:55:21 -0500

    Beginning to add new items.

---
+++ Content/Armor/ScoutHeadI.cs
@@ -0,0 +1,12 @@
+﻿using Terraria.ModLoader;
+
+namespace deeprockitems.Content.Armor
+{
+    public class ScoutHeadI : ModItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+        }
+    }
+}
