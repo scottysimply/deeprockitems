df613beb5f8ea7ebe3fb8992c046cc04b17502e1
scottsimply <53203532+scottysimply@users.noreply.github.com>
2023-12-15 10:22:57 -0600

    Collision elpers

---
+++ Utilities/CollisionHelpers.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace deeprockitems.Utilities
+{
+    public static class CollisionHelpers
+    {
+        /// <summary>
+        /// Performs <see cref="Collision.CanHitLine(Vector2, int, int, Vector2, int, int)"/> with only 2 entities.
+        /// </summary>
+        /// <param name="entity1"></param>
+        /// <param name="entity2"></param>
+        /// <returns></returns>
+        public static bool CanHitLine(Entity entity1, Entity entity2)
+        {
+            return Collision.CanHitLine(entity1.position, entity1.width, entity1.height, entity2.position, entity2.width, entity2.height);
+        }
+    }
+}
