ff491df19b47a0960a2ec604b7701c96d4febede
scottsimply <53203532+scottysimply@users.noreply.github.com>
2023-12-14 21:38:51 -0600

    Cryo-tastic!

---
+++ Content/Projectiles/ZhukovProjectiles/CryoMineletProjectile.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace deeprockitems.Content.Projectiles.ZhukovProjectiles
+{
+    public class CryoMineletProjectile : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = Projectile.height = 8;
+            Projectile.velocity = Vector2.Zero;
+            Projectile.tileCollide = false;
+        }
+        private float armingTimer { get => Projectile.ai[0]; set => Projectile.ai[0] = value; }
+    }
+}
