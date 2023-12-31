af4fb3c937b2fe696e0cdf24a68512d995958e62
scottsimply <53203532+scottysimply@users.noreply.github.com>
2023-12-31 15:06:46 -0600

    Assets file

---
+++ Assets/Textures/DRGTextures.cs
@@ -0,0 +1,11 @@
+﻿using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace deeprockitems.Assets.Textures
+{
+    public class DRGTextures
+    {
+        public static Texture2D InventorySlot = ModContent.Request<Texture2D>("deeprockitems/Assets/Textures/InventorySlot").Value;
+        public static Texture2D SlotOutline = ModContent.Request<Texture2D>("deeprockitems/Assets/Textures/SlotOutline").Value;
+    }
+}
