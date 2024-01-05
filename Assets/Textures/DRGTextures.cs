f724c8ebccb65f189bcd319b3df9c6428b3c9aa7
scottsimply <53203532+scottysimply@users.noreply.github.com>
2024-01-05 10:37:32 -0600

    Changed textures to be get properties

--- Assets/Textures/DRGTextures.cs
+++ Assets/Textures/DRGTextures.cs
@@ -1,11 +1,17 @@
 ﻿using Terraria.ModLoader;
 using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
 
 namespace deeprockitems.Assets.Textures
 {
-    public class DRGTextures
+    public class DRGTextures : ModSystem
     {
-        public static Texture2D InventorySlot = ModContent.Request<Texture2D>("deeprockitems/Assets/Textures/InventorySlot").Value;
-        public static Texture2D SlotOutline = ModContent.Request<Texture2D>("deeprockitems/Assets/Textures/SlotOutline").Value;
+        public override void SetStaticDefaults()
+        {
+            InventorySlot = ModContent.Request<Texture2D>("deeprockitems/Assets/Textures/InventorySlot", AssetRequestMode.ImmediateLoad).Value;
+            SlotOutline = ModContent.Request<Texture2D>("deeprockitems/Assets/Textures/SlotOutline", AssetRequestMode.ImmediateLoad).Value;
+        }
+        public static Texture2D InventorySlot { get; private set; }
+        public static Texture2D SlotOutline { get; private set; }
     }
 }
