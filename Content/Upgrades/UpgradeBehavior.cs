0924d26840ebccc9f3a8c25225829256298cb8e9
scottsimply <scottistriggered@gmail.com>
2024-12-03 12:58:52 -0600

    Changed upgrade behavior to utilize delegates instead of Func<> and Action<>

--- Content/Upgrades/UpgradeBehavior.cs
+++ Content/Upgrades/UpgradeBehavior.cs
@@ -8,20 +8,31 @@ namespace deeprockitems.Content.Upgrades
 {
     public class UpgradeBehavior
     {
-        public Action<Item> Item_ModifyStats { get; set; }
-        public Action<Item, Player> Item_HoldItemHook { get; set; }
-        public Action<Projectile, IEntitySource> Projectile_OnSpawnHook { get; set; }
-        public Action<Projectile> Projectile_AIHook { get; set; }
-        public Action<Projectile, NPC, NPC.HitInfo, int> Projectile_OnHitNPCHook { get; set; }
-        public Func<Projectile, Color, bool> Projectile_PreDrawHook { get; set; }
-        public Func<Projectile, NPC, NPC.HitModifiers, NPC.HitModifiers> Projectile_ModifyHitNPCHook { get; set; }
-        public ProjectilePreKill Projectile_PreKillHook { get; set; }
+        #region Delegates for upgrade behavior
+        public delegate void ItemStatChange(Item item);
+        public delegate void ItemHoldItem(Item item, Player player);
+        public delegate void ProjectileOnSpawn(Projectile projectile, IEntitySource source);
+        public delegate void ProjectileAI(Projectile projectile);
+        public delegate void ProjectileOnHitNPC(Projectile projectile, NPC target, NPC.HitInfo hit, int damageDone);
+        public delegate bool ProjectilePreDraw(Projectile projectile, Color lightColor);
+        public delegate void ProjectileModifyHitNPC(Projectile projectile, NPC target, ref NPC.HitModifiers modifiers);
+        public delegate void ItemOnShoot(Item item, Player player, EntitySource_FromUpgradableWeapon source, Projectile projectile);
         public delegate bool ProjectilePreKill(Projectile projectile, int timeLeft);
-        // Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback
-        public Action<Item, Player, EntitySource_FromUpgradableWeapon, Projectile> Item_OnShoot { get; set; }
         public delegate bool ProjectileOnTileCollide(Projectile projectile, Vector2 oldVelocity);
-        public ProjectileOnTileCollide Projectile_OnTileCollideHook { get; set; }
         public delegate void ItemModifyShootStats(Item item, Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback, ref float spread);
+        #endregion
+        #region Handlers for upgrade behavior
+        public ItemStatChange Item_ModifyStats { get; set; }
+        public ItemHoldItem Item_HoldItemHook { get; set; }
+        public ProjectileOnSpawn Projectile_OnSpawnHook { get; set; }
+        public ProjectileAI Projectile_AIHook { get; set; }
+        public ProjectileOnHitNPC Projectile_OnHitNPCHook { get; set; }
+        public ProjectilePreDraw Projectile_PreDrawHook { get; set; }
+        public ProjectileModifyHitNPC Projectile_ModifyHitNPCHook { get; set; }
+        public ProjectilePreKill Projectile_PreKillHook { get; set; }
+        public ItemOnShoot Item_OnShootHook { get; set; }
+        public ProjectileOnTileCollide Projectile_OnTileCollideHook { get; set; }
         public ItemModifyShootStats Item_ModifyShootStatsHook { get; set; }
+        #endregion
     }
 }
