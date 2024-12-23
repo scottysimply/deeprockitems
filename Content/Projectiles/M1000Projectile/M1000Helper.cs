9f8c1039c3780e02de6a48db4da214d83d77ad96
scottsimply <scottistriggered@gmail.com>
2024-12-23 08:30:32 -0600

    Beginning to debug how high velocity bullets work with the m1000

--- Content/Projectiles/M1000Projectile/M1000Helper.cs
+++ Content/Projectiles/M1000Projectile/M1000Helper.cs
@@ -8,16 +8,17 @@ namespace deeprockitems.Content.Projectiles.M1000Projectile
     public class M1000Helper : HeldProjectileBase
     {
         public override float ChargeTime { get; set; } = 30f;
-        public override int ProjectileToSpawn { get; set; } = ProjectileID.BulletHighVelocity;
         public override SoundStyle? ChargeSound => DRGSoundIDs.M1000Focus;
         public override SoundStyle? FireSound => DRGSoundIDs.M1000Fire;
         public override void NewSetDefaults() {
             ChargeShotCooldownMultiplier = 2f;
         }
         public override void ModifyProjectileAfterSpawning(Projectile projectile) {
-            if ((ammoUsed != ItemID.MusketBall || ammoUsed != ItemID.EndlessMusketPouch) && projectile.maxPenetrate == 5) return;
-
-            projectile.penetrate = 1;
+            /*if (ProjectileToSpawn == ProjectileID.Bullet)
+            {
+                ProjectileToSpawn = ProjectileID.BulletHighVelocity;
+                projectile.penetrate = projectile.maxPenetrate = 1;
+            }*/
         }
         public override void WhenReachedFullCharge()
         {
