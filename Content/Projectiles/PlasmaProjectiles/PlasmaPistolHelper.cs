7d8c7ce22d34f3e1dc00b9490c599d5f09ffac7c
scottsimply <scottistriggered@gmail.com>
2024-12-26 12:28:00 -0600

    Made it so that the first shot out of the EPC following a large projectile will have 100% accuracy. This is to add spread, but keep Thin Containment Field strong.

--- Content/Projectiles/PlasmaProjectiles/PlasmaPistolHelper.cs
+++ Content/Projectiles/PlasmaProjectiles/PlasmaPistolHelper.cs
@@ -2,16 +2,17 @@
 using Terraria;
 using Terraria.Audio;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
 
 namespace deeprockitems.Content.Projectiles.PlasmaProjectiles
 {
     public class PlasmaPistolHelper : HeldProjectileBase
     {
-        public override double Spread => 0;
         public override int ProjectileToSpawn { get; set; } = ModContent.ProjectileType<PlasmaBullet>();
         public override SoundStyle? ChargeSound { get; set; } = SoundID.Item117;
         public override SoundStyle? FireSound { get; set; } = SoundID.Item114;
         public override float ChargeTime { get; set; } = 45f;
+        private static bool _noSpreadOnNextShot = false;
         public override void WhenReachedFullCharge()
         {
             ProjectileToSpawn = ModContent.ProjectileType<BigPlasma>();
@@ -20,6 +21,14 @@ namespace deeprockitems.Content.Projectiles.PlasmaProjectiles
             Spread = 0;
             FireSound = SoundID.Item105;
             Cooldown = 4;
+            _noSpreadOnNextShot = true;
+        }
+        public override void ModifyShootStats(Item item, Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback, ref float spread) {
+            if (_noSpreadOnNextShot && type == ModContent.ProjectileType<PlasmaBullet>())
+            {
+                spread = 0;
+                _noSpreadOnNextShot = false;
+            }
         }
         public override void ModifyProjectileAfterSpawning(Projectile projectile) {
             if (ProjectileToSpawn == ModContent.ProjectileType<BigPlasma>())
