4f26464f3af8a05957a72b9dc48d9e4338f99970
scottsimply <scottistriggered@gmail.com>
2025-03-12 16:57:03 -0500

    Data class added for overclocks

---
+++ Content/Upgrades/Overclock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.Localization;
+
+namespace deeprockitems.Content.Upgrades
+{
+    public class Overclock
+    {
+        public Upgrade InternalUpgrade { get; set; }
+        public LocalizedText[] Positives { get; set; }
+        public LocalizedText[] Negatives { get; set; }
+    }
+}
