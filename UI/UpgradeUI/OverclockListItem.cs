c0a820f8a49a723aee5ad8a11010b4b1fa048da0
scottsimply <scottistriggered@gmail.com>
2025-04-03 21:59:39 -0500

    Switched over to UIList, i might as well

---
+++ UI/UpgradeUI/OverclockListItem.cs
@@ -0,0 +1,45 @@
+﻿using deeprockitems.Content.Upgrades;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent.UI.Elements;
+using Terraria.ModLoader.UI;
+using Terraria.UI;
+
+namespace deeprockitems.UI.UpgradeUI
+{
+    public class OverclockListItem : UIPanel
+    {
+        public UIText Label { get; set; }
+        public UIImage Icon { get; set; }
+        private Overclock _internalOverclock;
+        public OverclockListItem(Overclock overclock) {
+            _internalOverclock = overclock;
+        }
+        public override void OnInitialize() {
+            Label = new UIText(_internalOverclock.DisplayName, textScale: 0.75f);
+            Icon = new UIImage(_internalOverclock.Texture.Value);
+            Icon.Left.Pixels = 2f;
+            Icon.Height.Pixels = 32f;
+            Icon.Width.Pixels = 32f;
+            Label.Left.Pixels = Icon.Left.Pixels + 6f;
+            Icon.Activate();
+            Label.Activate();
+            Height.Pixels = 40f;
+            Width.Pixels = Width.Pixels - 20f;
+            Append(Icon);
+            Append(Label);
+        }
+        public override void Draw(SpriteBatch spriteBatch) {
+            base.Draw(spriteBatch);
+            if (ContainsPoint(Main.MouseScreen)) {
+                UICommon.TooltipMouseText($"[c/E3B465:{_internalOverclock.DisplayName}]\n" +
+                                          $"{_internalOverclock.HoverText}");
+            }
+        }
+    }
+}
--- UI/UpgradeUI/OverclockMenu.cs
+++ UI/UpgradeUI/OverclockMenu.cs
@@ -2,9 +2,12 @@
 using deeprockitems.UI.Components;
 using ReLogic.Content;
 using System;
+using System.Linq;
 using Terraria;
 using Terraria.GameContent.UI.Elements;
+using Terraria.Localization;
 using Terraria.ModLoader;
+using Terraria.ModLoader.UI;
 using Terraria.UI;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -12,26 +15,42 @@ namespace deeprockitems.UI.UpgradeUI
 {
     public class OverclockMenu : UIPanel
     {
+        public UIScrollbar Scrollbar { get; set; }
         public UIText OverclockLabel { get; set; }
-        public ListView<Upgrade> OverclockList { get; set; }
-        public OverclockMenu(string text_for_label) {
-            OverclockLabel = new(text_for_label);
+        public UIList OverclockList { get; set; }
+        public override void OnInitialize() {
+            base.OnInitialize();
+            OverclockLabel = new(Language.GetOrRegister("Mods.deeprockitems.Misc.UsefulWords.Overclocks", () => "Overclocks"), textScale: 0.66f) {
+                Left = { Percent = 0f, Pixels = -4f }
+            };
+            Append(OverclockLabel);
+            Scrollbar = new UIScrollbar {
+                Left = { Percent = 1f, Pixels = -14f },
+                Height = { Percent = 1f, Pixels = -OverclockLabel.Height.Pixels },
+                Top = { Pixels = OverclockLabel.Height.Pixels }
+            };
+            OverclockList = new UIList {
+                Width = { Pixels = -20, Percent = 1f },
+                Left = { Percent = 0f, Pixels = 20f },
+                Top = { Pixels = OverclockLabel.Height.Pixels },
+                Height = { Percent = 1f, Pixels = -OverclockLabel.Height.Pixels},
+            };
+            OverclockList.SetScrollbar(Scrollbar);
+            Append(Scrollbar);
+            Append(OverclockList);
         }
         public void SetOverclocks(UpgradeTier tier) {
-            OverclockList = new([..tier], (upgrade) => {
-                UIPanel display = new();
-                display.Width.Percent = 0.99f;
-                UIText text = new UIText(upgrade.DisplayName);
-                display.Height = text.MinHeight;
-                display.Append(text);
-                return display;
-            });
+            if (tier is null || tier.Tier != UpgradeBuilder.OVERCLOCK_TIER)
+            {
+                OverclockList.Clear();
+                return;
+            }
+            // Generate overclock elements from tier
+            var list_of_elements = tier.Select<Upgrade, OverclockListItem>(upgrade => new(upgrade as Overclock));
+            OverclockList.AddRange(list_of_elements);
         }
         public void RemoveOverclocks() {
             OverclockList = null;
-        }
-        public override void OnInitialize() {
-            
         }
     }
 }
