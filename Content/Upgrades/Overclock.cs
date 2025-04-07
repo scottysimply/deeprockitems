ce127b9cbf4205f1aeea239aa9f3034d70830229
scottsimply <scottistriggered@gmail.com>
2025-04-07 09:44:28 -0500

    Added backgrounds to overclocks

--- Content/Upgrades/Overclock.cs
+++ Content/Upgrades/Overclock.cs
@@ -11,8 +11,28 @@ namespace deeprockitems.Content.Upgrades
 {
     public class Overclock : Upgrade
     {
-        public Overclock(string internalName, Asset<Texture2D> sprite) : base(internalName, sprite) {
-            
+        public enum OverclockType {
+            Unset = 0,
+            Clean = 1,
+            Balanced = 2,
+            Unstable = 3,
+        }
+        public Overclock(string internalName, Asset<Texture2D> sprite, OverclockType overclockType) : base(internalName, sprite) {
+            _type = overclockType;
+        }
+        public OverclockType Type { get => _type; }
+        private readonly OverclockType _type;
+        public override Asset<Texture2D> Background
+        {
+            get
+            {
+                return _type switch {
+                    OverclockType.Clean => Assets.Upgrades.Backgrounds.Clean,
+                    OverclockType.Balanced => Assets.Upgrades.Backgrounds.Balanced,
+                    OverclockType.Unstable => Assets.Upgrades.Backgrounds.Unstable,
+                    _ => Assets.WhitePixel,
+                };
+            }
         }
         public LocalizedText Positives { get => Language.GetOrRegister($"{LocalizedKey}.Positives", () => "Positives"); }
         public LocalizedText Negatives { get => Language.GetOrRegister($"{LocalizedKey}.Negatives", () => "Negatives"); }
