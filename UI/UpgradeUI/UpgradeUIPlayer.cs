f69b1af8cab361dd7aab0d89b0f928ebdaec0c1a
scottsimply <scottistriggered@gmail.com>
2025-05-04 08:41:57 -0500

    Removed some commented out code. Tons of unused code actually needs to be removed

--- UI/UpgradeUI/UpgradeUIPlayer.cs
+++ UI/UpgradeUI/UpgradeUIPlayer.cs
@@ -25,21 +25,6 @@ namespace deeprockitems.UI.UpgradeUI
                 Player.QuickSpawnItem(ItemToSpawnOnWorldLoad.GetSource_ReleaseEntity(), ItemToSpawnOnWorldLoad);
             }
         }
-        /*public override void SaveData(TagCompound tag) {
-            tag.Add("UpgradeSlotItem", ItemInSlot);
-            ItemInSlot = new Item(0);
-            _upgradeSystem.UpgradeUIState.Panel.UpgradeContainer.SetUpgrades(null);
-        }
-        public override void LoadData(TagCompound tag) {
-            if (tag.ContainsKey("UpgradeSlotItem") && tag.Get<Item>("UpgradeSlotItem") != null)
-            {
-                var itemInSlot = tag.Get<Item>("UpgradeSlotItem");
-                // Prepare to spawn
-                ItemToSpawnOnWorldLoad = itemInSlot;
-                // Remove key
-                tag.Remove("UpgradeSlotItem");
-            }
-        }*/
         private static UpgradeSystem _upgradeSystem;
         public Item ItemToSpawnOnWorldLoad = new(0);
         public Item ItemInSlot { get => _upgradeSystem.UpgradeUIState.Panel.ParentSlot.ItemInSlot; set => _upgradeSystem.UpgradeUIState.Panel.ParentSlot.ItemInSlot = value; }
