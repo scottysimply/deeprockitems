dd4ecb7a06b32c0ad57a69f402c62fa4cffb450f
scottsimply <53203532+scottysimply@users.noreply.github.com>
2025-05-06 15:26:54 -0500

    Created overclock summary element

---
+++ UI/UpgradeUI/OverclockUI/OverclockSummary.cs
@@ -0,0 +1,41 @@
+﻿using deeprockitems.Content.Upgrades;
+using deeprockitems.Types;
+using Microsoft.Xna.Framework.Graphics;
+using System.Linq;
+using Terraria.GameContent.UI.Elements;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ModLoader.UI;
+using Terraria.UI;
+
+namespace deeprockitems.UI.UpgradeUI.OverclockUI
+{
+    public class OverclockSummary : UIPanel
+    {
+        public UIText OverclockLabel { get; set; }
+        public Overclock? CurrentOverclock { get; set; }
+        public RectangleF OverclockDrawLocation { get; set; }
+        public UIButton<LocalizedText> OverclockStateButton { get; set; }
+        public override void OnInitialize() {
+            base.OnInitialize();
+            OverclockLabel = new(Language.GetOrRegister("Mods.deeprockitems.Misc.UsefulWords.Overclock", () => "Overclock"), textScale: 0.66f) {
+                Left = { Percent = 0f, Pixels = -4f }
+            };
+            OverclockLabel.OnLeftClick += (UIMouseEvent evt, UIElement sender) => {
+                ModContent.GetInstance<UpgradeSystem>().UpgradeUIState.SetState<OverclockPanel>();
+            };
+            Append(OverclockLabel);
+            OverclockStateButton = new(Language.GetOrRegister("Mods.deeprockitems.Misc.UsefulWords.ViewOverclocks", () => "View Overclocks")) {
+                Left = { Percent = 0f },
+                Top = { Pixels = OverclockLabel.Height.Pixels }
+            };
+            OverclockStateButton.OnLeftClick += (UIMouseEvent evt, UIElement sender) => {
+                ModContent.GetInstance<UpgradeSystem>().UpgradeUIState.SetState<OverclockPanel>();
+            };
+            Append(OverclockStateButton);
+        }
+        protected override void DrawSelf(SpriteBatch spriteBatch) {
+            
+        }
+    }
+}
