8ee2582af6530ce435d1ca3307cef371145d7204
scottsimply <scottistriggered@gmail.com>
2025-05-11 22:33:26 -0500

    Added selection menu and details

--- UI/UpgradeUI/OverclockUI/OverclockPanel.cs
+++ UI/UpgradeUI/OverclockUI/OverclockPanel.cs
@@ -1,7 +1,7 @@
-﻿using deeprockitems.Content.Items.Misc;
+﻿using deeprockitems.Content.Items;
+using deeprockitems.Content.Items.Misc;
+using deeprockitems.Content.Upgrades;
 using Terraria;
-using Terraria.GameContent.UI.Elements;
-using Terraria.ModLoader;
 using Terraria.UI;
 
 namespace deeprockitems.UI.UpgradeUI.OverclockUI
@@ -9,16 +9,45 @@ namespace deeprockitems.UI.UpgradeUI.OverclockUI
     public class OverclockPanel : UpgradePanel
     {
         public FakeItemSlot MatrixCoreSlot { get; set; }
+        public OverclockDetails Details { get; set; }
+        public OverclockSelectionMenu SelectionMenu { get; set; }
         public override void PostInitialize() {
+            int PADDING = 6;
             MatrixCoreSlot = new FakeItemSlot((mouseItem, slotItem) => {
                 if (mouseItem.ModItem is MatrixCore) return true;
-                if (mouseItem.type == 0 & slotItem.type > 0) return true;
+                if (slotItem.type != 0 && (mouseItem.type == 0 || mouseItem.ModItem is MatrixCore)) return true;
                 return false;
-            });
-            MatrixCoreSlot.Left = new StyleDimension { Pixels = ParentSlot.Left.Pixels + ParentSlot.Width.Pixels };
-            MatrixCoreSlot.Top = ParentSlot.Top;
-            MatrixCoreSlot.Width = MatrixCoreSlot.Height = new StyleDimension { Pixels = 52 };
+            }) {
+                Left = { Pixels = ParentSlot.Left.Pixels + ParentSlot.Width.Pixels + PADDING },
+                Top = ParentSlot.Top,
+                Width = { Pixels = 52 },
+                Height = { Pixels = 52 }
+            };
             Append(MatrixCoreSlot);
+            SelectionMenu = new OverclockSelectionMenu() {
+                Width = { Percent = 0.5f, Pixels = -PADDING },
+                Height = { Percent = 1f, Pixels = -MatrixCoreSlot.Height.Pixels - PADDING },
+                Left = { Percent = 0f },
+                Top = { Pixels = MatrixCoreSlot.Height.Pixels + PADDING}
+            };
+            Append(SelectionMenu);
+            SelectionMenu.Activate();
+            Details = new OverclockDetails() {
+                Width = { Percent = 0.5f, Pixels = -PADDING },
+                Height = { Percent = 1f, Pixels = -MatrixCoreSlot.Height.Pixels - PADDING },
+                Left = { Percent = 0.5f },
+                Top = { Pixels = MatrixCoreSlot.Height.Pixels + PADDING }
+            };
+            Append(Details);
+            SelectionMenu.SetOverclocks((ParentSlot.ItemInSlot.ModItem as IUpgradable)?.UpgradeMasterList[UpgradeBuilder.OVERCLOCK_TIER] ?? null);
+        }
+        protected override void OnClickParentSlot(Item itemNowInSlot, Item itemThatLeftSlot) {
+            if (itemNowInSlot.ModItem is IUpgradable modItem)
+            {
+                SelectionMenu.SetOverclocks(modItem.UpgradeMasterList[UpgradeBuilder.OVERCLOCK_TIER] ?? null);
+                return;
+            }
+            SelectionMenu.SetOverclocks(null);
         }
         protected override void OnClickForgeButton(UIMouseEvent evt, UIElement sender) {
             if (MatrixCoreSlot.ItemInSlot?.type != 0)
