6cd5ef34757bed51b4d3029d8686e24836455271
scottsimply <scottistriggered@gmail.com>
2025-05-11 22:28:58 -0500

    Added "details" ui element

---
+++ UI/UpgradeUI/OverclockUI/OverclockDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Terraria;
+using Terraria.GameContent.UI.Elements;
+using Terraria.UI;
+
+namespace deeprockitems.UI.UpgradeUI.OverclockUI
+{
+    public class OverclockDetails : UIPanel
+    {
+        private Item _item;
+        public override void OnInitialize() {
+            base.OnInitialize();
+        }
+    }
+}
