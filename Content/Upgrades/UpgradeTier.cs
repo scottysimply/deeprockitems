9173d8484946e518e5cfc574073e2a9cba4ca2d9
scottsimply <scottistriggered@gmail.com>
2025-05-14 20:47:53 -0500

    Set the upgrade's tiers through the tier constructor

--- Content/Upgrades/UpgradeTier.cs
+++ Content/Upgrades/UpgradeTier.cs
@@ -10,16 +10,41 @@ namespace deeprockitems.Content.Upgrades
         {
             _innerArray = data;
             Tier = tier;
+            foreach (var upgrade in _innerArray)
+            {
+                upgrade.Tier = this;
+            }
         }
         public Upgrade this[int i]
         {
             get => _innerArray[i];
         }
         public int Length => _innerArray.Length;
-        public int SelectedUpgradeIndex { get; private set; }
+        public int SelectedIndex { get; private set; }
+        /// <summary>
+        /// Obtains the selected upgrade, or null if no upgrade is selected.
+        /// </summary>
+        public Upgrade? SelectedUpgrade { get => SelectedIndex == -1 ? null : _innerArray[SelectedIndex]; }
         public void SelectUpgrade(int index)
         {
-            SelectedUpgradeIndex = index;
+            if (index >= _innerArray.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "The provided index is greater than the length of the tier.");
+            }
+            else if (index < -1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), $"The provided index is less than -1. To specify no selection, {nameof(index)} should be -1.");
+            }
+            SelectedIndex = index;
+            for (int i = 0; i < _innerArray.Length; i++)
+            {
+                if (i == index)
+                {
+                    _innerArray[index].UpgradeState.IsEquipped = true;
+                    continue;
+                }
+                _innerArray[i].UpgradeState.IsEquipped = false;
+            }
         }
         private Upgrade[] _innerArray;
         public int Tier { get; set; }
