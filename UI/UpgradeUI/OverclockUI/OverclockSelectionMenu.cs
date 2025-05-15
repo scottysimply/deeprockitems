f8aa25d9d1941fe5cf80966edf07b598df852bab
scottsimply <scottistriggered@gmail.com>
2025-05-14 20:50:54 -0500

    Fixed the overclock list not displaying its elements. Its height was 0 :(

--- UI/UpgradeUI/OverclockUI/OverclockSelectionMenu.cs
+++ UI/UpgradeUI/OverclockUI/OverclockSelectionMenu.cs
@@ -1,6 +1,7 @@
 ﻿using deeprockitems.Content.Upgrades;
 using Microsoft.Xna.Framework.Graphics;
 using System.Linq;
+using Terraria;
 using Terraria.GameContent.UI.Elements;
 using Terraria.Localization;
 using Terraria.ModLoader;
@@ -10,16 +11,17 @@ namespace deeprockitems.UI.UpgradeUI.OverclockUI
 {
     public class OverclockSelectionMenu : UIPanel
     {
+        public OverclockService SelectedOverclock { get; set; }
         public UIScrollbar Scrollbar { get; set; }
         public UIText OverclockLabel { get; set; }
         public UIList OverclockList { get; set; }
+        public OverclockSelectionMenu(OverclockService overclock) {
+            SelectedOverclock = overclock;
+        }
         public override void OnInitialize() {
             base.OnInitialize();
             OverclockLabel = new(Language.GetOrRegister("Mods.deeprockitems.Misc.UsefulWords.Overclocks", () => "Overclocks"), textScale: 0.66f) {
                 Left = { Percent = 0f, Pixels = -4f }
-            };
-            OverclockLabel.OnLeftClick += (UIMouseEvent evt, UIElement sender) => {
-                ModContent.GetInstance<UpgradeSystem>().UpgradeUIState.SetState<OverclockPanel>();
             };
             Append(OverclockLabel);
             Scrollbar = new UIScrollbar {
@@ -31,13 +33,22 @@ namespace deeprockitems.UI.UpgradeUI.OverclockUI
             OverclockList = new UIList {
                 Width = { Pixels = -20f, Percent = 1f },
                 Top = { Pixels = OverclockLabel.GetDimensions().Height + 4f },
-                Height = { Pixels = this.Height.Pixels - OverclockLabel.Height.Pixels - 30f },
-                OverflowHidden = true,
+                Height = { Percent = 1f, Pixels = -OverclockLabel.GetDimensions().Height },
             };
+            OverclockList.OnLeftClick += OverclockList_OnLeftClick;
             OverclockList.SetScrollbar(Scrollbar);
             Append(Scrollbar);
             Append(OverclockList);
         }
+
+        private void OverclockList_OnLeftClick(UIMouseEvent evt, UIElement listeningElement) {
+            if (evt.Target is OverclockListItem target)
+            {
+                Main.NewText($"Selected {target.ThisOverclock.DisplayName}");
+                SelectedOverclock.ThisOverclock = target.ThisOverclock;
+            }
+        }
+
         public void SetOverclocks(UpgradeTier tier) {
             if (tier is null || tier.Tier != UpgradeBuilder.OVERCLOCK_TIER)
             {
@@ -45,15 +56,13 @@ namespace deeprockitems.UI.UpgradeUI.OverclockUI
                 return;
             }
             // Generate overclock elements from tier
-            var list_of_elements = tier.Select<Upgrade, OverclockListItem>(upgrade => new(tier, upgrade as Overclock) {
+            var list_of_elements = tier.Select<Upgrade, OverclockListItem>(upgrade => new(upgrade as Overclock) {
+                Width = { Percent = 1f, Pixels = -10f },
                 Height = { Pixels = 40f },
-                Width = { Pixels = 40f },
-                Left = { Pixels = 6f }
+                Left = { Pixels = 0f },
             });
             OverclockList.AddRange(list_of_elements);
-        }
-        protected override void DrawChildren(SpriteBatch spriteBatch) {
-            base.DrawChildren(spriteBatch);
+            OverclockList.Activate();
         }
         public void RemoveOverclocks() {
             OverclockList = null;
