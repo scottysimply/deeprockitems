e89ad5350e67bd8fce6c28a861c1df9fb0391527
scottsimply <scottistriggered@gmail.com>
2025-05-14 20:46:50 -0500

    Added a service handler to allow Overclocks to be passed between UI components

---
+++ Content/Upgrades/OverclockService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deeprockitems.Content.Upgrades
+{
+    public class OverclockService
+    {
+        public Overclock ThisOverclock { get; set; }
+    }
+}
