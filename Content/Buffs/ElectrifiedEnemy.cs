c3033cac10e3c50bc3b4240065db70f7f941f6a9
scottsimply <scottistriggered@gmail.com>
2025-05-20 19:59:24 -0500

    Electrified enemy debuff should hopefully tick even after being applied.

--- Content/Buffs/ElectrifiedEnemy.cs
+++ Content/Buffs/ElectrifiedEnemy.cs
@@ -24,49 +24,47 @@ namespace deeprockitems.Content.Buffs
         {
             IsElectrified = false;
         }
+        private int buffDamageTimer = 0;
         public override void AI(NPC npc)
         {
-            // If npc electirifed
-            if (IsElectrified)
+            if (!IsElectrified)
             {
-                // Find buff index
-                int buffIndex = npc.FindBuffIndex(ModContent.BuffType<ElectrifiedEnemy>());
-                if (buffIndex != -1)
+                buffDamageTimer = 0;
+                return;
+            }
+            buffDamageTimer++;
+            // Determine if buff should tick
+            if (buffDamageTimer % 60 == 0)
+            {
+                // Get adjusted whoAmI
+                int adjustedWhoAmI = npc.whoAmI;
+                if (npc.realLife >= 0)
                 {
-                    // Determine if buff should tick
-                    if (npc.buffTime[buffIndex] % 60 == 0)
+                    adjustedWhoAmI = npc.realLife;
+                }
+
+                // If npc can be damaged
+                if (!Main.npc[adjustedWhoAmI].immortal)
+                {
+                    // Tick buff
+                    Main.npc[adjustedWhoAmI].life -= 30;
+                }
+                // Spawn dust
+                Dust.NewDust(npc.Center, 8, 8, DustID.Electric, SpeedX: Main.rand.NextFloat() - 0.5f, SpeedY: Main.rand.NextFloat() - 0.5f, Scale: 0.8f);
+
+                // Spawn combattext
+                CombatText.NewText(new Rectangle((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height), CombatText.LifeRegenNegative, 15, dramatic: false, dot: true);
+
+                // Kill NPC if life is 0.
+                if (Main.npc[adjustedWhoAmI].life <= 0)
+                {
+                    Main.npc[adjustedWhoAmI].life = 1;
+                    if (Main.netMode != NetmodeID.MultiplayerClient)
                     {
-                        // Get adjusted whoAmI
-                        int adjustedWhoAmI = npc.whoAmI;
-                        if (npc.realLife >= 0)
+                        Main.npc[adjustedWhoAmI].StrikeInstantKill();
+                        if (Main.netMode == NetmodeID.Server)
                         {
-                            adjustedWhoAmI = npc.realLife;
-                        }
-
-                        // If npc can be damaged
-                        if (!Main.npc[adjustedWhoAmI].immortal)
-                        {
-                            // Tick buff
-                            Main.npc[adjustedWhoAmI].life -= 30;
-                        }
-                        // Spawn dust
-                        Dust.NewDust(npc.Center, 8, 8, DustID.Electric, SpeedX: Main.rand.NextFloat() - 0.5f, SpeedY: Main.rand.NextFloat() - 0.5f, Scale: 0.8f);
-
-                        // Spawn combattext
-                        CombatText.NewText(new Rectangle((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height), CombatText.LifeRegenNegative, 15, dramatic: false, dot: true);
-
-                        // Kill NPC if life is 0.
-                        if (Main.npc[adjustedWhoAmI].life <= 0)
-                        {
-                            Main.npc[adjustedWhoAmI].life = 1;
-                            if (Main.netMode != NetmodeID.MultiplayerClient)
-                            {
-                                Main.npc[adjustedWhoAmI].StrikeInstantKill();
-                                if (Main.netMode == NetmodeID.Server)
-                                {
-                                    NetMessage.SendData(MessageID.DamageNPC, -1, -1, null, adjustedWhoAmI, 9999f);
-                                }
-                            }
+                            NetMessage.SendData(MessageID.DamageNPC, -1, -1, null, adjustedWhoAmI, 9999f);
                         }
                     }
                 }
