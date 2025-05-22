8142a98b329f4784e5f0a73d2893c91b425ae6b6
scottsimply <53203532+scottysimply@users.noreply.github.com>
2025-05-21 22:44:02 -0500

    Fixed up embedded detonators code.

--- Common/NPCs/EmbeddedDetsNPC.cs
+++ Common/NPCs/EmbeddedDetsNPC.cs
@@ -9,12 +9,22 @@ namespace deeprockitems.Common.NPCs
     public class EmbeddedDetsNPC : GlobalNPC
     {
         public override bool InstancePerEntity => true;
-        public int EmbeddedCount { get; set; } = 0;
-        public void ExplodeThisNPC(NPC npc, Player player)
+        int _detonatorCount = 0;
+        public void IncrementDetonators() {
+            _detonatorCount++;
+        }
+        /// <summary>
+        /// Tries to detonate the detonators on a given NPC. Returns true if it was successful
+        /// </summary>
+        /// <param name="npc"></param>
+        /// <param name="player"></param>
+        public bool TryDetonateOnNPC(NPC npc, Player player)
         {
+            if (_detonatorCount <= 0) return false;
             SoundEngine.PlaySound(SoundID.DD2_KoboldExplosion, position: npc.Center);
-            player.StrikeNPCDirect(npc, new NPC.HitInfo() { Damage = (int)Math.Floor(150 + 400*Math.Log10(EmbeddedCount)), Knockback = 1f });
-            EmbeddedCount = 0;
+            player.StrikeNPCDirect(npc, new NPC.HitInfo() { Damage = (int)Math.Floor(150 + 400*Math.Log10(_detonatorCount)), Knockback = 1f });
+            _detonatorCount = 0;
+            return true;
         }
     }
 }
