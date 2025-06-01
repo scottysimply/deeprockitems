c02edc7f385d06664d508f11ed9a2cf1a499fa5d
scottsimply <53203532+scottysimply@users.noreply.github.com>
2025-06-01 10:03:21 -0500

    Added hover text and fixed frame drawing

--- UI/UpgradeUI/OverclockUI/OverclockIcon.cs
+++ UI/UpgradeUI/OverclockUI/OverclockIcon.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader.UI;
 using Terraria.UI;
 
 namespace deeprockitems.UI.UpgradeUI.OverclockUI
@@ -19,8 +21,29 @@ namespace deeprockitems.UI.UpgradeUI.OverclockUI
             if (_overclock is not null)
             {
                 var dims = GetDimensions().ToRectangle();
-                spriteBatch.Draw(_overclock.Background.Value, dims.Center.ToVector2(), Color.White);
-                spriteBatch.Draw(_overclock.Texture.Value, dims.Center.ToVector2(), Color.White);
+                var overclockFrame = _overclock.Background.Frame(verticalFrames: 3, frameY: (int)_overclock.Type - 1);
+                spriteBatch.Draw(_overclock.Background.Value, dims.Center.ToVector2() - 0.5f * overclockFrame.Size(), overclockFrame, Color.White, 0f, new Vector2(0.5f), 1f, SpriteEffects.None, 0f);
+                spriteBatch.Draw(_overclock.Texture.Value, dims.Center.ToVector2() - 0.5f * _overclock.Texture.Size(), null, Color.White, 0f, new Vector2(0.5f), 1f, SpriteEffects.None, 0f);
+                if (GetDimensions().ToRectangle().Contains(Main.MouseScreen.ToPoint()))
+                {
+                    string mouseText = $"[c/E3B465:{Language.GetTextValue("Mods.deeprockitems.Misc.UsefulWords.Overclock")}: {_overclock.DisplayName}]\n" +
+                                       $"{_overclock.HoverText}";
+                    if (_overclock.Positives.Value != _overclock.Positives.Key)
+                    {
+                        foreach (var line in _overclock.Positives.Value.Split('\n'))
+                        {
+                            mouseText += $"\n[c/4ec726:▲ {line}]";
+                        }
+                    }
+                    if (_overclock.Negatives.Value != _overclock.Negatives.Key)
+                    {
+                        foreach (var line in _overclock.Negatives.Value.Split('\n'))
+                        {
+                            mouseText += $"\n[c/db2121:▼ {line}]";
+                        }
+                    }
+                    UICommon.TooltipMouseText(mouseText);
+                }
             }
         }
         public void SetOverclock(Overclock overclock) {
