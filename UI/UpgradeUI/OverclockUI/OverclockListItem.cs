b86266b1fe71525a3566ea90975a990343b42a07
scottsimply <53203532+scottysimply@users.noreply.github.com>
2025-06-06 14:50:22 -0500

    Fixed draw scale for overclock list items

--- UI/UpgradeUI/OverclockUI/OverclockListItem.cs
+++ UI/UpgradeUI/OverclockUI/OverclockListItem.cs
@@ -3,9 +3,11 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using Terraria;
+using Terraria.GameContent;
 using Terraria.GameContent.UI.Elements;
 using Terraria.ModLoader.UI;
 using Terraria.UI;
+using Terraria.UI.Chat;
 
 namespace deeprockitems.UI.UpgradeUI.OverclockUI
 {
@@ -17,7 +19,13 @@ namespace deeprockitems.UI.UpgradeUI.OverclockUI
             ThisOverclock = overclock;
         }
         public override void OnInitialize() {
-            OverclockName = new(ThisOverclock.DisplayName) {
+            float scale = 1f;
+            Vector2 measuredSize = ChatManager.GetStringSize(FontAssets.MouseText.Value, ThisOverclock.DisplayName.Value, Vector2.One);
+            if (measuredSize.X > GetInnerDimensions().Width)
+            {
+                scale = this.GetInnerDimensions().Width / measuredSize.X;
+            }
+            OverclockName = new(ThisOverclock.DisplayName, textScale: scale) {
                 IgnoresMouseInteraction = true
             };
             Append(OverclockName);
