acd7d8ac4156fbf1e995711f36edf21deca6fc10
scottsimply <53203532+scottysimply@users.noreply.github.com>
2025-06-07 11:54:33 -0500

    Fixed localization bug involving positive/negative overclocks

--- Content/Upgrades/Overclock.cs
+++ Content/Upgrades/Overclock.cs
@@ -18,7 +18,7 @@ namespace deeprockitems.Content.Upgrades
         public OverclockType Type { get => _type; }
         private readonly OverclockType _type;
         public override Asset<Texture2D> Background => Assets.Upgrades.Overclocks.Backgrounds;
-        public LocalizedText Positives { get => Language.GetOrRegister($"{LocalizedKey}.Positives", () => "Positives"); }
-        public LocalizedText Negatives { get => Language.GetOrRegister($"{LocalizedKey}.Negatives", () => "Negatives"); }
+        public LocalizedText Positives { get => Language.GetOrRegister($"{LocalizedKey}.Positives"); }
+        public LocalizedText Negatives { get => Language.GetOrRegister($"{LocalizedKey}.Negatives"); }
     }
 }
