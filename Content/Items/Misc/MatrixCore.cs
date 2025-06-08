89dbd4954f4b00c6f5660f2cc8b9a14234eb0940
scottsimply <53203532+scottysimply@users.noreply.github.com>
2025-06-08 17:30:53 -0500

    Added a recipe for blank matrix cores and allowed them to be imbued

--- Content/Items/Misc/MatrixCore.cs
+++ Content/Items/Misc/MatrixCore.cs
@@ -2,11 +2,13 @@
 using Terraria.GameContent.Creative;
 using Terraria.ModLoader;
 using Terraria.ID;
+using deeprockitems.Content.Upgrades;
 
 namespace deeprockitems.Content.Items.Misc
 {
     public class MatrixCore : ModItem
     {
+        public Overclock? InfusedOverclock { get; set; }
         public override void SetStaticDefaults()
         {
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 3;
@@ -18,6 +20,12 @@ namespace deeprockitems.Content.Items.Misc
             Item.rare = ItemRarityID.Orange;
             Item.value = Item.buyPrice(0, 2, 50, 0);
             Item.maxStack = Item.CommonMaxStack;
+        }
+        public override void AddRecipes() {
+            Recipe.Create(Type)
+                .AddRecipeGroup(nameof(ItemID.IronBar), 8)
+                .AddIngredient(ItemID.Glass, 6)
+                .AddIngredient(ItemID.Diamond, 2);
         }
     }
 }
