1158dd050861aa70b976303acaf2a68f3a81d8cf
scottsimply <53203532+scottysimply@users.noreply.github.com>
2025-06-09 09:34:34 -0500

    Added infusing logic to the matrix core. Unsure of how to change the texture, though

--- Content/Items/Misc/MatrixCore.cs
+++ Content/Items/Misc/MatrixCore.cs
@@ -3,12 +3,35 @@ using Terraria.GameContent.Creative;
 using Terraria.ModLoader;
 using Terraria.ID;
 using deeprockitems.Content.Upgrades;
+using System;
+using System.Collections.Generic;
+using Terraria.Localization;
+using deeprockitems.Utilities;
 
 namespace deeprockitems.Content.Items.Misc
 {
     public class MatrixCore : ModItem
     {
-        public Overclock? InfusedOverclock { get; set; }
+        public bool IsInfused { get => _infusedOverclock != null; }
+        private Overclock _infusedOverclock;
+        public Overclock InfusedOverclock { get => _infusedOverclock; }
+        public void InfuseWthOverclock(Overclock overclock) {
+            if (overclock is null) throw new ArgumentNullException("Matrix cores cannot be set with null!");
+            _infusedOverclock = overclock;
+        }
+        public override LocalizedText DisplayName => IsInfused ? Language.GetOrRegister("Mods.deeprockitems.Items.MatrixCore.InfusedDisplayName") : base.DisplayName;
+        public override void ModifyTooltips(List<TooltipLine> tooltips) {
+            int index = tooltips.FindIndex(t => t.Name == "Tooltip0");
+            if (index <= -1) return;
+            // if not infused, add the blank tooltip
+            if (!IsInfused)
+            {
+                tooltips.Insert(index, new TooltipLine(Mod, "BlankMatrixTooltip", Language.GetOrRegister("Mods.deeprockitems.Items.MatrixCore.BlankTooltip").Value));
+                return;
+            }
+
+            tooltips.Insert(index, new TooltipLine(Mod, "InfusedMatrixTooltip", Language.GetOrRegister("Mods.deeprockitems.Items.MatrixCore.InfusedTooltip").Value));
+        }
         public override void SetStaticDefaults()
         {
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 3;
