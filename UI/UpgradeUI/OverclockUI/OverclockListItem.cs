22017beec9d173d2920d5a40e43e827e038b25b7
scottsimply <53203532+scottysimply@users.noreply.github.com>
2025-07-04 08:49:10 -0500

    Added an icon to each overclock list element

--- UI/UpgradeUI/OverclockUI/OverclockListItem.cs
+++ UI/UpgradeUI/OverclockUI/OverclockListItem.cs
@@ -14,19 +14,31 @@ namespace deeprockitems.UI.UpgradeUI.OverclockUI
     public class OverclockListItem : UIPanel
     {
         public Overclock ThisOverclock;
+        public OverclockIcon Icon { get; set; }
         public UIText OverclockName { get; set; }
         public OverclockListItem(Overclock overclock) {
             ThisOverclock = overclock;
         }
         public override void OnInitialize() {
-            float scale = 1f;
+            var innerDims = GetInnerDimensions();
+            Icon = new();
+            Icon.Width.Pixels = Icon.Height.Pixels = GetDimensions().ToRectangle().Height;
+            Icon.Left.Pixels = -10f;
+            Icon.Top.Pixels = 4f;
+            Icon.SetOverclock(ThisOverclock);
+            Icon.ShowTooltip = false;
+            Append(Icon);
+
+
+            float nameScale = 1f;
             Vector2 measuredSize = ChatManager.GetStringSize(FontAssets.MouseText.Value, ThisOverclock.DisplayName.Value, Vector2.One);
-            if (measuredSize.X > GetInnerDimensions().Width)
+            if (measuredSize.X > innerDims.Width - Icon.Width.Pixels + 8f)
             {
-                scale = this.GetInnerDimensions().Width / measuredSize.X;
+                nameScale = (innerDims.Width - Icon.Width.Pixels + 8f) / measuredSize.X;
             }
-            OverclockName = new(ThisOverclock.DisplayName, textScale: scale) {
-                IgnoresMouseInteraction = true
+            OverclockName = new(ThisOverclock.DisplayName, textScale: nameScale) {
+                IgnoresMouseInteraction = true,
+                Left = { Pixels = Icon.Width.Pixels - 4f }
             };
             Append(OverclockName);
         }
