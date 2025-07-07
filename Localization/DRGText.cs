528b666fdca1a341947b94f37c6a3335846120bc
scottsimply <53203532+scottysimply@users.noreply.github.com>
2025-07-06 22:06:00 -0500

    Added comments because it's hard to glance at what is happening

--- Localization/DRGText.cs
+++ Localization/DRGText.cs
@@ -8,6 +8,7 @@ namespace deeprockitems.Localization
         public static Color PositiveText { get => new(0x4E, 0xC7, 0x26); }
         public static Color NegativeText { get => new(0xF1, 0x30, 0x10); }
         public static string TextColor(this string str, Color color) {
+            // Prepend the text coloring
             string begin = $"[c/{color.Hex3()}:";
             string msg = begin;
             for (int i = 0; i < str.Length; i++)
@@ -17,6 +18,7 @@ namespace deeprockitems.Localization
                     msg += str[i];
                     continue;
                 }
+                // If the text contains a closing bracket, add the coloring again
                 msg += $"]{begin}]";
             }
             msg += ']';
