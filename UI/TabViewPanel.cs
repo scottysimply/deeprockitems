9c2a2ab114ae898005eb74e5fb76c664a0313009
scottsimply <53203532+scottysimply@users.noreply.github.com>
2025-07-15 18:35:05 -0500

    Made the tabview take in a generic UIPanel type (for the sake of retrieving data more efficiently)

--- UI/TabViewPanel.cs
+++ UI/TabViewPanel.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria;
 using Terraria.GameContent;
 using Terraria.GameContent.UI.Elements;
 using Terraria.Localization;
@@ -15,59 +16,74 @@ using Terraria.UI.Chat;
 
 namespace deeprockitems.UI
 {
-    public class TabViewPanel : UIElement {
+    public class TabViewPanel<TPanel> : UIElement where TPanel : UIPanel {
         private string _selectedText = "";
-        private Dictionary<string, UIPanel> _views;
+        private Dictionary<string, TPanel> _views;
         private bool _needsRevalidate = false;
-        public TabViewPanel(float labelHeight) {
-            LabelHeight = labelHeight;
+        public TabViewPanel() {
             _views = [];
         }
-        private UIPanel SelectedPanel
+#nullable enable
+        public TPanel? SelectedPanel
         {
             get
             {
-                if (_views.TryGetValue(_selectedText, out UIPanel panel))
+                if (_views.TryGetValue(_selectedText, out TPanel? panel))
                 {
                     return panel;
                 }
-                return new UIPanel();
+                return null;
             }
         }
+#nullable disable
         public float LabelHeight { get; private set; }
-        public void AddPanel(LocalizedText label, UIPanel panel) {
+        public void SetLabelHeight(float labelHeight) {
+            LabelHeight = labelHeight;
+            _needsRevalidate = true;
+        }
+        public void AddPanel(LocalizedText label, TPanel panel) {
             AddPanel(label.Value, panel);
         }
-        public void AddPanel(string label, UIPanel panel) {
+        public void AddPanel(string label, TPanel panel) {
             _views.Add(label, panel);
             _needsRevalidate = true;
         }
         private void ValidateLabelsAndPanel() {
             // Remove existing labels/panels
-            foreach (var child in Children)
+            List<UIElement> newChildren = [..Children];
+            foreach (var child in newChildren)
             {
                 if (child is TabLabel label)
                 {
                     label.IsSelected = false;
                     RemoveChild(child);
+                    child.Deactivate();
                 }
                 else if (child is UIPanel)
                 {
                     RemoveChild(child);
+                    child.Deactivate();
                 }
             }
 
             // Create new labels
-            float tabX = 0f;
+            float tabX = 20f;
             foreach ((string text, UIPanel panel) in _views)
             {
                 TabLabel label = new TabLabel(text) {
                     Left = { Pixels = tabX},
-                    Height = { Pixels = LabelHeight }
+                    Height = { Pixels = LabelHeight },
+                    Top = { Pixels = 2f }
                 };
+                label.IsSelected = false;
+                // Makes sure the panel stays selected
+                if ((_selectedText ?? "") == text)
+                {
+                    label.IsSelected = true;
+                }
                 Append(label);
-                label.IsSelected = true;
-                tabX += label.Width.Pixels;
+                label.Activate();
+                tabX += label.Width.Pixels + 8f;
             }
 
             // Check panel
@@ -78,8 +94,10 @@ namespace deeprockitems.UI
             SelectedPanel.Top.Pixels = LabelHeight;
             SelectedPanel.Height.Pixels = GetInnerDimensions().Height - LabelHeight;
             Append(SelectedPanel);
+            SelectedPanel.Activate();
         }
         public override void OnInitialize() {
+            _selectedText = _views.Keys.First();
             ValidateLabelsAndPanel();
         }
         public override void Update(GameTime gameTime) {
@@ -88,9 +106,10 @@ namespace deeprockitems.UI
                 ValidateLabelsAndPanel();
                 _needsRevalidate = false;
             }
+            base.Update(gameTime);
         }
         public override void LeftMouseDown(UIMouseEvent evt) {
-            if (evt.Target is TabLabel label)
+            if (evt.Target is TabLabel label && label.Text != _selectedText)
             {
                 // Deselect all others, select this one
                 foreach (var child in Children)
@@ -101,43 +120,52 @@ namespace deeprockitems.UI
                     }
                 }
                 label.IsSelected = true;
+                _selectedText = label.Text;
+                _needsRevalidate = true;
             }
         }
         public override void Draw(SpriteBatch spriteBatch) {
-            // Draw panels first
+            // Separate children into their own parts
             List<TabLabel> labels = [];
+            List<UIElement> otherChildren = [];
             foreach (UIElement child in Children)
             {
-                // Exclude labels for now
                 if (child is TabLabel label)
                 {
                     labels.Add(label);
                     continue;
                 }
-                child.Draw(spriteBatch);
+                otherChildren.Add(child);
             }
-            // draw selected label last
+            // Order will go labels -> other children -> the selected label
             TabLabel selectedLabel = null;
             foreach (TabLabel label in labels)
             {
+                // save the selected label for last
                 if (label.IsSelected)
                 {
                     selectedLabel = label;
                     continue;
                 }
+                // normal label
                 label.Draw(spriteBatch);
             }
+            // draw normal children
+            foreach (var child in otherChildren)
+            {
+                child.Draw(spriteBatch);
+            }
+            // draw selected last (over everything else)
             selectedLabel?.Draw(spriteBatch);
         }
         protected class TabLabel : UIElement {
             private float _labelHeight;
-            private string _text;
             public Color BorderColor = Color.Black;
             public TabLabel(LocalizedText text) : this(text.Value) {
 
             }
             public TabLabel(string text) {
-                _text = text;
+                Text = text;
             }
             private Color BackgroundColor 
             {
@@ -147,16 +175,23 @@ namespace deeprockitems.UI
                     {
                         return new Color(63, 82, 151) * 0.7f;
                     }
-                    return new Color((int)(63 * 0.75f), (int)(82 * 0.75f), (int)(151 * 0.75f)) * 0.7f;
+                    float dimPercent = 0.66f;
+                    return new Color((int)(63 * dimPercent), (int)(82 * dimPercent), (int)(151 * dimPercent)) * 0.8f;
                 }
             }
             public bool IsSelected { get; set; }
+            public string Text { get; private set; }
             public UIText LabelText { get; set; }
             public override void OnInitialize() {
-                Vector2 baseSize = ChatManager.GetStringSize(FontAssets.MouseText.Value, _text, new(1f));
+                Vector2 baseSize = ChatManager.GetStringSize(FontAssets.MouseText.Value, Text, new(1f));
                 float scale = GetInnerDimensions().Height / baseSize.Y;
-                LabelText = new(_text, scale);
-                Width.Pixels = baseSize.X * scale;
+                LabelText = new(Text, scale) {
+                    PaddingLeft = 18,
+                    PaddingRight = 18,
+                    Top = { Pixels = 6}
+                };
+                LabelText.IgnoresMouseInteraction = true;
+                Width.Pixels = baseSize.X * scale + 36;
                 Append(LabelText);
             }
             protected override void DrawSelf(SpriteBatch spriteBatch) {
@@ -169,7 +204,7 @@ namespace deeprockitems.UI
             /// </summary>
             public void DrawTab(SpriteBatch spriteBatch, Texture2D texture, int insetWidth, int insetHeight, Vector2 position, float width, float height, Color color) {
                 // Draw each edge
-                Rectangle dimensions = new Rectangle((int)position.X, (int)position.Y, (int)width, (int)height);
+                var dimensions = new Rectangle((int)position.X, (int)position.Y, (int)width, (int)height);
                 // Left
                 spriteBatch.Draw(texture, new Rectangle(dimensions.X, dimensions.Y + insetHeight, insetWidth, dimensions.Height - 2 * insetHeight), new Rectangle(0, insetHeight, insetWidth, texture.Height - 2 * insetHeight), color);
                 // Right
