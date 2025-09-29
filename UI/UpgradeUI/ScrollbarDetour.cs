75d907ba4fb1c8fa3f8b75ac0617065a8a2f0b5b
scottsimply <53203532+scottysimply@users.noreply.github.com>
2025-09-28 21:29:48 -0500

    Added a detour to fix a god forsaken bug with scrollbars :)

---
+++ UI/UpgradeUI/ScrollbarDetour.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.GameContent.UI.Elements;
+using Terraria.ModLoader;
+
+namespace deeprockitems.UI.UpgradeUI
+{
+    public class ScrollbarDetour : ModSystem
+    {
+        public override void Load() {
+            On_UIScrollbar.SetView += On_UIScrollbar_SetView;
+        }
+
+        private void On_UIScrollbar_SetView(On_UIScrollbar.orig_SetView orig, UIScrollbar self, float viewSize, float maxViewSize) {
+            if (maxViewSize == 0)
+            {
+                maxViewSize = 1f;
+            }
+            orig(self, viewSize, maxViewSize);
+        }
+    }
+}
