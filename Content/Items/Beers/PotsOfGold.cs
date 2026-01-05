d1ea96994e88c596b2e839c7a26cfc6b90ee8fe7
scottsimply <53203532+scottysimply@users.noreply.github.com>
2026-01-04 21:20:39 -0600

    Added pots of gold; did not implement

---
+++ Content/Items/Beers/PotsOfGold.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace deeprockitems.Content.Items.Beers
+{
+    public class PotsOfGold : BaseBeer
+    {
+        public override void SetBuff() {
+            Item.buffType = ModContent.BuffType<PogBuff>();
+        }
+        public class PogBuff : BuffForBeer {
+
+        }
+    }
+}
