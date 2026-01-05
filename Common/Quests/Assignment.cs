61913800e08efbb0e487b6d83a574cfb8e971412
scottsimply <53203532+scottysimply@users.noreply.github.com>
2026-01-04 21:17:42 -0600

    Added assignment class to represent assignments for quests

---
+++ Common/Quests/Assignment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deeprockitems.Common.Quests
+{
+    /// <summary>
+    /// Assignments are chains of multiple quests. Each assignment has a Queue of quests to complete in a given amount of time. Assignemnts refresh the morning after they are completed.
+    /// </summary>
+    public class Assignment
+    {
+        public Queue<Quest> Quests { get; set; }
+        public int DaysLeft { get; set; }
+    }
+}
