63ac3e29e0abcf6d39ed70d52e15077666f302bf
scottsimply <53203532+scottysimply@users.noreply.github.com>
2026-03-12 09:56:47 -0500

    Added iframe tracker for primtive projectiles

---
+++ Common/NPCs/PrimitiveImmunityNPC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace deeprockitems.Common.NPCs
+{
+    public class PrimitiveImmunityNPC : GlobalNPC
+    {
+        public override bool InstancePerEntity => true;
+        public Dictionary<int, int> StaticImmunityFrames;
+        public override void SetDefaults(NPC entity) {
+            StaticImmunityFrames = [];
+        }
+        public override bool PreAI(NPC npc) {
+            foreach (var timer in StaticImmunityFrames)
+            {
+                if (StaticImmunityFrames[timer.Key]-- <= 0)
+                {
+                    StaticImmunityFrames.Remove(timer.Key);
+                }
+            }
+            return base.PreAI(npc);
+        }
+    }
+}
